39aa5ee21ec4c39f3c5537b300806797e169096a
wangwenhan <wangwenhan@bytedance.com>
2020-06-14 12:27:55 +0800

    add cacheing pattern

--- src/main/java/caching/UserService.java
+++ src/main/java/caching/UserService.java
@@ -2,6 +2,8 @@ package caching;
 
 import lombok.SneakyThrows;
 
+import java.util.Objects;
+
 /**
  * @author : wangwenhan
  * @since : 2020/6/14
@@ -11,6 +13,18 @@ public class UserService {
     @SneakyThrows
     public static String queryUserName(String userId) {
         Thread.sleep(1000);
+        if(Objects.equals("1",userId)) {
+            return "黄圣依";
+        }
+        if(Objects.equals("2",userId)) {
+            return "伊能静";
+        }
+        if(Objects.equals("3",userId)) {
+            return "张雨绮";
+        }
+        if(Objects.equals("4",userId)) {
+            return "宁静";
+        }
         return "codog代码狗";
     }
 }
